3df9b70a3f18d8d0a88bdf05d755eb09e46b6aff
Jovan Bogdanović <152297482+bxgda@users.noreply.github.com>
2024-05-26 21:20:14 +0200

    Add files via upload

---
+++ lab5/lab5/PodaciOTeniseru.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace lab5
+{
+    partial class PodaciOTeniseru
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblIme = new System.Windows.Forms.Label();
+            this.lblPrezime = new System.Windows.Forms.Label();
+            this.lblRank = new System.Windows.Forms.Label();
+            this.lblDatum = new System.Windows.Forms.Label();
+            this.lblStil = new System.Windows.Forms.Label();
+            this.lblTitule = new System.Windows.Forms.Label();
+            this.lblPenzija = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prezime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Rank:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Datum rodjenja:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(90, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Stil igre:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Broj titula:";
+            // 
+            // lblIme
+            // 
+            this.lblIme.AutoSize = true;
+            this.lblIme.Location = new System.Drawing.Point(203, 47);
+            this.lblIme.Name = "lblIme";
+            this.lblIme.Size = new System.Drawing.Size(35, 13);
+            this.lblIme.TabIndex = 7;
+            this.lblIme.Text = "label8";
+            // 
+            // lblPrezime
+            // 
+            this.lblPrezime.AutoSize = true;
+            this.lblPrezime.Location = new System.Drawing.Point(203, 81);
+            this.lblPrezime.Name = "lblPrezime";
+            this.lblPrezime.Size = new System.Drawing.Size(35, 13);
+            this.lblPrezime.TabIndex = 8;
+            this.lblPrezime.Text = "label9";
+            // 
+            // lblRank
+            // 
+            this.lblRank.AutoSize = true;
+            this.lblRank.Location = new System.Drawing.Point(203, 115);
+            this.lblRank.Name = "lblRank";
+            this.lblRank.Size = new System.Drawing.Size(41, 13);
+            this.lblRank.TabIndex = 9;
+            this.lblRank.Text = "label10";
+            // 
+            // lblDatum
+            // 
+            this.lblDatum.AutoSize = true;
+            this.lblDatum.Location = new System.Drawing.Point(203, 147);
+            this.lblDatum.Name = "lblDatum";
+            this.lblDatum.Size = new System.Drawing.Size(41, 13);
+            this.lblDatum.TabIndex = 10;
+            this.lblDatum.Text = "label11";
+            // 
+            // lblStil
+            // 
+            this.lblStil.AutoSize = true;
+            this.lblStil.Location = new System.Drawing.Point(203, 179);
+            this.lblStil.Name = "lblStil";
+            this.lblStil.Size = new System.Drawing.Size(41, 13);
+            this.lblStil.TabIndex = 12;
+            this.lblStil.Text = "label13";
+            // 
+            // lblTitule
+            // 
+            this.lblTitule.AutoSize = true;
+            this.lblTitule.Location = new System.Drawing.Point(203, 207);
+            this.lblTitule.Name = "lblTitule";
+            this.lblTitule.Size = new System.Drawing.Size(41, 13);
+            this.lblTitule.TabIndex = 13;
+            this.lblTitule.Text = "label14";
+            // 
+            // lblPenzija
+            // 
+            this.lblPenzija.AutoSize = true;
+            this.lblPenzija.Location = new System.Drawing.Point(133, 248);
+            this.lblPenzija.Name = "lblPenzija";
+            this.lblPenzija.Size = new System.Drawing.Size(40, 13);
+            this.lblPenzija.TabIndex = 14;
+            this.lblPenzija.Text = "penzija";
+            // 
+            // PodaciOTeniseru
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 450);
+            this.Controls.Add(this.lblPenzija);
+            this.Controls.Add(this.lblTitule);
+            this.Controls.Add(this.lblStil);
+            this.Controls.Add(this.lblDatum);
+            this.Controls.Add(this.lblRank);
+            this.Controls.Add(this.lblPrezime);
+            this.Controls.Add(this.lblIme);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PodaciOTeniseru";
+            this.Text = "PodaciOTeniseru";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblIme;
+        private System.Windows.Forms.Label lblPrezime;
+        private System.Windows.Forms.Label lblRank;
+        private System.Windows.Forms.Label lblDatum;
+        private System.Windows.Forms.Label lblStil;
+        private System.Windows.Forms.Label lblTitule;
+        private System.Windows.Forms.Label lblPenzija;
+    }
+}
---
+++ lab5/lab5/PodaciOTeniseru.cs
@@ -0,0 +1,36 @@
+﻿using Podaci;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class PodaciOTeniseru : Form
+    {
+        public PodaciOTeniseru()
+        {
+            InitializeComponent();
+        }
+
+        public PodaciOTeniseru(Teniser t)
+        {
+            InitializeComponent();
+            lblIme.Text = t.Ime;
+            lblPrezime.Text = t.Prezime;
+            lblRank.Text = t.Rank.ToString();
+            lblDatum.Text = t.Datum.ToShortDateString();
+            lblStil.Text = t.StilIgre.ToString();
+            lblTitule.Text = t.Titule.ToString();
+            if (t.Penzija)
+                lblPenzija.Text = "penzionisan";
+            else
+                lblPenzija.Text = string.Empty;
+        }
+    }
+}
